41b0b5e6998cf0d457b798f20b76a38b94eebc98
camel-cased <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>
2023-10-27 17:58:58 +0300

    [Feat, WIP] add classes

---
+++ Classes/UIKit+PropertySetter/UIPageControl+PropertySetter.swift
@@ -0,0 +1,8 @@
+//
+//  UIPageControl+PropertySetter.swift
+//  Pods
+//
+//  Created by Oleksandr Kabanov on 27.10.2023.
+//
+
+import Foundation
---
+++ Classes/UIKit+PropertySetter/UIStackView+PropertySetter.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+PropertySetter.swift
+//  Pods
+//
+//  Created by Oleksandr Kabanov on 27.10.2023.
+//
+
+import Foundation
